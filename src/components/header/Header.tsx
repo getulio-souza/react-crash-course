6137f44144fa5339741e914b24ef495f9f6aae8d
Getulio Silva25 <gsilva25@lenovo.com>
2026-05-14 11:25:20 -0300

    header-with-icons

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,24 @@
 import "./Header.css";
 
+import tableIcon from "../../assets/table-icon.png"
+import formIcon from "../../assets/form-icon.png"
+
 function Header(){
+
+    function navigateToUsersTable(){}
+
+    function navigateToUsersForm(){}
+
     return(
         <>
         <section className="users-header-container">
-            <div className="users-category">
+            <div className="users-category" onClick={navigateToUsersTable}>
+                <img src={tableIcon} alt="" width={"30px"} />
                 <span>Users Table</span>
-                <img src="" alt="" />
             </div>
-            <div className="users-category">
+            <div className="users-category" onClick={navigateToUsersForm}>
+                <img src={formIcon} alt="" width={"30px"} />
                 <span>Users Form</span>
-                <img src="" alt="" />
             </div>
         </section>
         </>
